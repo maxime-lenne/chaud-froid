72958ddcd67c81d4f00d778514e8e901ff58fca2
Maxime Lenne <maxime.lenne@gmail.com>
2012-10-17 02:12:10 +0200

    modification de la couleur de fond en rapport avec la légende + bouton, function callback modifée

--- js/main.js
+++ js/main.js
@@ -19,10 +19,11 @@ function getCats() {
 	var geo = navigator.geolocation;
 	// Position du visiteur geolocalisation
 	if (geo) {
-		geo.getCurrentPosition(function(geoloc){
+		geo.watchPosition(function(geoloc){
 			catsPosition = new google.maps.LatLng(geoloc.coords.latitude, geoloc.coords.longitude);
 			console.log("Position du chat (geolocalisation HTML5)" + catsPosition);
-			
+			calculateDistance(mousePosition, catsPosition);
+			refreshCatsScreen();
 		}, function(){
 			// Google loader api
 			// require <script type="text/javascript" src="http://www.google.com/jsapi"></script>
@@ -48,16 +49,50 @@ function getCats() {
 function calculateDistance(address1, address2) {
 	mesure = (google.maps.geometry.spherical.computeDistanceBetween(address1, address2) / 1000).toFixed(2);
 	console.log("distance entre le client et le shop : " + mesure);
-	if(mesure < 15) {
-		console.log("dans le périmetre");
-		return true;
-	}
-	else {
-		console.log("pas dans le périmetre");
-		return false;
-  }
-	
-	
 }
 
-
+/*
+ * Modification ecran cats
+ *
+ */
+function refreshCatsScreen() {
+	//Array ( [1] => 114,145,221 [2] => 125,144,212 [3] => 135,143,203 [4] => 146,142,194 [5] => 157,141,185 [6] => 168,140,175 [7] => 179,139,166 [8] => 190,138,157 [9] => 200,137,148 [10] => 211,136,139 )
+	if (mesure >= 5000) {
+		$(".row-fluid").css("background-color", "rgb(114,145,221)");
+		$(".row-fluid .btn").removeClass("btn-danger");
+		$(".row-fluid .btn").addClass("btn-primary");
+	}
+	else if (mesure >= 1000 && mesure < 5000) {
+		$(".row-fluid").css("background-color", "rgb(125,144,212)");
+	}
+	else if (mesure >= 500 && mesure < 1000) {
+		$(".row-fluid").css("background-color", "rgb(135,143,203)");
+	}
+	else if (mesure >= 250 && mesure < 500){
+		$(".row-fluid").css("background-color", "rgb(146,142,194)");
+	}
+	else if (mesure >= 100 && mesure < 250) {
+		$(".row-fluid").css("background-color", "rgb(157,141,185)");
+	}
+	else if (mesure >= 75 && mesure < 100) {
+		$(".row-fluid").css("background-color", "rgb(168,140,175)");
+	}
+	else if (mesure >= 50 && mesure < 75) {
+		$(".row-fluid").css("background-color", "rgb(179,139,166)");
+	}
+	else if (mesure >= 25 && mesure < 50) {
+		$(".row-fluid").css("background-color", "rgb(190,138,157)");
+	}
+	else if (mesure >= 10 && mesure < 25) {
+		$(".row-fluid").css("background-color", "rgb(200,137,148)");
+	}
+	else if (mesure >= 5 && mesure < 10) {
+		$(".row-fluid").css("background-color", "rgb(211,136,139)");
+	}
+	else if (mesure < 5) {
+		$(".row-fluid").css("background-color", "rgb(211,136,139)");
+		$(".row-fluid .btn").removeClass("btn-primary");
+		$(".row-fluid .btn").addClass("btn-danger");
+	}
+	$(".row-fluid button").html(mesure);
+}
